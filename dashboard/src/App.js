77ccdd527e8743fc8664c1640c7528cc6d7f37d2
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-13 23:41:47 +0800

    chore: comment thing

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,3 +1,9 @@
+/**
+ * React Query set-up
+ * - Wrapping the children of the App component in a QueryClientProvider
+ * - Create a queryClient using new QueryClient
+ * - Pass queryClient as a value to client in the QueryClientProvider
+ */
 import './App.css';
 
 export default function App() {
