8b44f9059f09b6a047c02a3cf4de274a1ee9b3b0
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-14 00:26:41 +0800

    feat: adding logic to fetch user list from typicode

---
+++ dashboard/src/query-hooks/useUsers.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchUsers = () => {
+  return axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.data);
+};
+
+export default function useUsers() {
+  return useQuery("users", fetchUsers);
+}
