faa558e09bb7a913b9cda690738a71c6f23e43b0
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-15 23:24:47 +0800

    feat: adding isFetching logic

--- dashboard/src/pages/Dashboard.jsx
+++ dashboard/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useState } from "react";
+import { useIsFetching } from "react-query";
 import User from "../components/User";
 import Users from "../components/Users";
 
 export default function Dashboard() {
   const [selectedUser, setSelectedUser] = useState(null);
+  const isFetching = useIsFetching();
 
   return (
     <>
@@ -13,6 +15,10 @@ export default function Dashboard() {
       <hr />
 
       <User selectedUser={selectedUser} />
+
+      {isFetching ? (
+        <p style={{ position: "fixed", top: "0%", right: "0%" }}>Loading...</p>
+      ) : null}
     </>
   );
 }
