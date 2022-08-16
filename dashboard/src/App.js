2eac5b0d9e7b10e3c7c0e73116c8446545528995
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-16 23:08:10 +0800

    chore: comment thing

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -3,6 +3,11 @@
  * - Wrapping the children of the App component in a QueryClientProvider
  * - Create a queryClient using new QueryClient
  * - Pass queryClient as a value to client in the QueryClientProvider
+ * 
+ * React Query - Set Data
+ * - POST request with React Query
+ * - Invalidate queries( invalidateQueries )
+ * - Update queries( setQueryData )
  */
 import { QueryClientProvider, QueryClient } from "react-query";
 import Dashboard from "./pages/Dashboard";
